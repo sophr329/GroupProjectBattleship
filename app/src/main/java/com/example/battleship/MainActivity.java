4c615ad6d9c73d2a9992fe3dc81947e1ee980fb8
sophr329 <84166707+sophr329@users.noreply.github.com>
2021-10-21 11:03:41 -0500

    tried to figure out 2d arrray

--- app/src/main/java/com/example/battleship/MainActivity.java
+++ app/src/main/java/com/example/battleship/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.battleship;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+     private ImageButton[][] spacesArray = ImageButton[][]{
+             for(int i = 0; i < 7; i++){
+                 for(int k = 0; k < 1; k++){
+                     spacesArray[i][k] = ;
+                 }
+             }
+    }
+
+
+
+
+
+
 }
