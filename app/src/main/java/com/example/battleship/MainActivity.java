86069c377c5d0cfdbcb630ae10646d53ca5f0abc
sophr329 <84166707+sophr329@users.noreply.github.com>
2021-10-22 11:00:49 -0500

    figured out stuff

--- app/src/main/java/com/example/battleship/MainActivity.java
+++ app/src/main/java/com/example/battleship/MainActivity.java
@@ -5,7 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageButton;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    int min = 1;
+    int max = 6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +20,15 @@ public class MainActivity extends AppCompatActivity {
 
      private ImageButton[][] spacesArray = ImageButton[][]{
              for(int i = 0; i < 7; i++){
-                 for(int k = 0; k < 1; k++){
+                 for(int k = 0; k < 7; k++){
                      spacesArray[i][k] = ;
                  }
              }
     }
 
+    public void ships(){
+
+    }
 
 
 
---
+++ app/src/main/java/com/example/battleship/Ships.java
@@ -0,0 +1,6 @@
+package com.example.battleship;
+
+public class Ships
+{
+    
+}
