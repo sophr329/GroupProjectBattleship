6f9e3adacd091f34c2d9f32c7d93ab4e308d065f
sophr329 <84166707+sophr329@users.noreply.github.com>
2021-10-27 11:04:23 -0500

    class work

--- app/src/main/java/com/example/battleship/MainActivity.java
+++ app/src/main/java/com/example/battleship/MainActivity.java
@@ -7,49 +7,59 @@ import android.view.View;
 import android.widget.ImageButton;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity<i> extends AppCompatActivity {
 
-    ImageButton buttonID;
-    ImageButton buttonIDtest;
+//    ImageButton buttonID;
+//    ImageButton buttonIDtest;
     Boolean ship;
     Boolean hitStatus;
+
+    private ImageButton [][] spacesArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        buttonID = (ImageButton) findViewById(R.id.button00);
-        buttonID.setOnClickListener(imgA);
+        spacesArray =  new ImageButton[8][8];
+        for (int x = 0; x < 8; x++) {
+            for (int k = 0; k < 8; k++) {
+                String buttID = "button" + x + k;
+                int id = getResources().getIdentifier(buttID, "id", getPackageName());
+                spacesArray[x][k] = findViewById(id);
 
-        buttonIDtest = (ImageButton) findViewById(R.id.button01);
-        buttonIDtest.setOnClickListener(imgB);
+            }
+        }
+
+//        buttonID = (ImageButton) findViewById(R.id.button00);
+//        buttonID.setOnClickListener(imgA);
+//
+//        buttonIDtest = (ImageButton) findViewById(R.id.button01);
+//        buttonIDtest.setOnClickListener(imgB);
     }
 
-    View.OnClickListener imgA = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            buttonID.setImageResource(R.drawable.x);
-        }
-    };
-
-    View.OnClickListener imgB = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            buttonIDtest.setImageResource(R.drawable.check);
-        }
-    };
-
-
-
-//    public void changeImage(boolean hitStatus){
-//        if(hitStatus && ship){
-//
-//
+//    View.OnClickListener imgA = new View.OnClickListener() {
+//        @Override
+//        public void onClick(View view) {
+//            buttonID.setImageResource(R.drawable.x);
 //        }
-//        else if(hitStatus){
+//    }
 //
+//    View.OnClickListener imgB = new View.OnClickListener() {
+//        @Override
+//        public void onClick(View view) {
+//            buttonIDtest.setImageResource(R.drawable.check);
 //        }
-//}
+//    }
+    
+    public void changeImage(boolean hitStatus, boolean ship){
+        if(hitStatus && ship){
+
+
+        }
+        else if(hitStatus){
+
+        }
+}
 
 }
