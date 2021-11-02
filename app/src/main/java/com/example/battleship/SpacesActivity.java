97620ec6b88fca4176410f381747bb80cde12561
sophr329 <84166707+sophr329@users.noreply.github.com>
2021-11-02 11:06:36 -0500

    class work

--- app/src/main/java/com/example/battleship/SpacesActivity.java
+++ app/src/main/java/com/example/battleship/SpacesActivity.java
@@ -12,6 +12,14 @@ public class SpacesActivity extends AppCompatActivity {
     private Spaces[][] spacesArray;
     int i;
 
+    private View.OnClickListener buttonOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            checkSpaces(v);
+        }
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +30,13 @@ public class SpacesActivity extends AppCompatActivity {
         for(int i = 0; i < spacesArray.length; i++) {
             for (int j = 0; j < spacesArray[0].length; j++) {
                 spacesArray[i][j] = new Spaces();
+                // you can' use v because there is no v in the onCreate
+                // need to figure out a way to programmatically access ALL ImageButtons in xml
+
+                spacesArray[i][j].setImgButton(findViewById(v.getId()));
+
+                // I don't thinkyou need listener
+                spacesArray[i][j].getImgButton().setOnClickListener(buttonOnClickListener);
             }
         }
 
