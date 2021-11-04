4dffcec0535889a884c58a06f86ed49ed402fdd8
sophr329 <84166707+sophr329@users.noreply.github.com>
2021-11-04 11:04:44 -0500

    random layout of ships made

--- app/src/main/java/com/example/battleship/SpacesActivity.java
+++ app/src/main/java/com/example/battleship/SpacesActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 
 public class SpacesActivity extends AppCompatActivity {
@@ -25,86 +26,152 @@ public class SpacesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        spacesArray =  new Spaces[7][7];
+        spacesArray = new Spaces[7][7];
 
-        for(int i = 0; i < spacesArray.length; i++) {
+        for (int i = 0; i < spacesArray.length; i++) {
             for (int j = 0; j < spacesArray[0].length; j++) {
-                String id = ""+ i+j;
+                String id = "" + i + j;
                 spacesArray[i][j] = new Spaces(id);
                 // you can' use v because there is no v in the onCreate
                 // need to figure out a way to programmatically access ALL ImageButtons in xml
 
-               // spacesArray[i][j].setImgButton(findViewById(v.getId()));
+                // spacesArray[i][j].setImgButton(findViewById(v.getId()));
 
                 // I don't thinkyou need listener
-              //  spacesArray[i][j].getImgButton().setOnClickListener(buttonOnClickListener);
+                //  spacesArray[i][j].getImgButton().setOnClickListener(buttonOnClickListener);
             }
         }
 
         placeShips();
 
-}
-
-    public void placeShips(){
-
-        spacesArray[0][0].setShip(true);
-        spacesArray[0][1].setShip(true);
-      //  spacesArray[0][2].setShip(true);
-      //  spacesArray[0][3].setShip(true);
     }
 
-    public void checkSpaces(View v){
+    public void placeShips() {
+        shipArrange();
+
+    }
+
+    public void shipArrange(){
+        int min = 1;
+        int max = 5;
+        int rand = (int)Math.floor(Math.random()*(max-min+1)+min);
+
+
+        if(rand == 1){
+            spacesArray[0][0].setShip(true);
+            spacesArray[1][0].setShip(true);
+            spacesArray[2][0].setShip(true);
+
+            spacesArray[3][2].setShip(true);
+            spacesArray[3][3].setShip(true);
+
+            spacesArray[5][1].setShip(true);
+            spacesArray[5][2].setShip(true);
+            spacesArray[5][3].setShip(true);
+
+            spacesArray[1][5].setShip(true);
+            spacesArray[2][5].setShip(true);
+            spacesArray[3][5].setShip(true);
+            spacesArray[4][5].setShip(true);
+        }
+        else if(rand == 2){
+            spacesArray[0][1].setShip(true);
+            spacesArray[0][2].setShip(true);
+            spacesArray[0][3].setShip(true);
+            spacesArray[0][4].setShip(true);
+
+            spacesArray[3][0].setShip(true);
+            spacesArray[4][0].setShip(true);
+
+            spacesArray[4][2].setShip(true);
+            spacesArray[5][2].setShip(true);
+            spacesArray[6][2].setShip(true);
+
+            spacesArray[2][5].setShip(true);
+            spacesArray[3][5].setShip(true);
+            spacesArray[4][5].setShip(true);
+        }
+        else if(rand == 3){
+            spacesArray[0][0].setShip(true);
+            spacesArray[1][0].setShip(true);
+
+            spacesArray[2][2].setShip(true);
+            spacesArray[2][3].setShip(true);
+            spacesArray[2][4].setShip(true);
+
+            spacesArray[6][0].setShip(true);
+            spacesArray[6][1].setShip(true);
+            spacesArray[6][2].setShip(true);
+
+            spacesArray[3][6].setShip(true);
+            spacesArray[4][6].setShip(true);
+            spacesArray[5][6].setShip(true);
+            spacesArray[6][6].setShip(true);
+        }
+        else if(rand == 4){
+            spacesArray[0][0].setShip(true);
+            spacesArray[1][0].setShip(true);
+
+            spacesArray[2][2].setShip(true);
+            spacesArray[2][3].setShip(true);
+            spacesArray[2][4].setShip(true);
+
+            spacesArray[6][0].setShip(true);
+            spacesArray[6][1].setShip(true);
+            spacesArray[6][2].setShip(true);
+
+            spacesArray[3][6].setShip(true);
+            spacesArray[4][6].setShip(true);
+            spacesArray[5][6].setShip(true);
+            spacesArray[6][6].setShip(true);
+        }
+        else{
+            spacesArray[3][0].setShip(true);
+            spacesArray[4][0].setShip(true);
+            spacesArray[5][0].setShip(true);
+
+            spacesArray[2][2].setShip(true);
+            spacesArray[3][2].setShip(true);
+            spacesArray[4][2].setShip(true);
+
+            spacesArray[1][4].setShip(true);
+            spacesArray[2][4].setShip(true);
+            spacesArray[3][4].setShip(true);
+            spacesArray[4][4].setShip(true);
+
+            spacesArray[6][4].setShip(true);
+            spacesArray[6][5].setShip(true);
+        }
+
+
+    }
+
+    public void checkSpaces(View v) {
         ImageButton img = findViewById(v.getId());
         String imgStr = img.getContentDescription().toString();
 
-         int row, col;
-         row = parseInt(imgStr.substring(6,7));
-         col = parseInt(imgStr.substring(7,8));
+        int row, col;
+        row = parseInt(imgStr.substring(6, 7));
+        col = parseInt(imgStr.substring(7, 8));
 
-         Spaces spaceSelected = spacesArray[row][col];
+        Spaces spaceSelected = spacesArray[row][col];
 
-         if(spaceSelected.isShip() && !spaceSelected.isHitStatus())
-         {
-             img.setImageResource(R.drawable.newcheck);
-             spaceSelected.setHitStatus(true);
-         }
-         else if(spaceSelected.isShip() && spaceSelected.isHitStatus())
-         {
-             //toast
-         }
-         else if(!spaceSelected.isShip() && !spaceSelected.isHitStatus()){
+        if (spaceSelected.isShip() && !spaceSelected.isHitStatus()) {
+            img.setImageResource(R.drawable.newcheck);
+            spaceSelected.setHitStatus(true);
+        } else if (spaceSelected.isShip() && spaceSelected.isHitStatus()) {
+            Toast.makeText(getApplicationContext(), "You already picked this square", Toast.LENGTH_SHORT).show();
+        } else if (!spaceSelected.isShip() && !spaceSelected.isHitStatus()) {
 
-             img.setImageResource(R.drawable.x);
-             spaceSelected.setHitStatus(true);
-         }
-         else
-         {
-             //toast
-         }
-
+            img.setImageResource(R.drawable.x);
+            spaceSelected.setHitStatus(true);
+        } else {
+            Toast.makeText(getApplicationContext(), "You already picked this square", Toast.LENGTH_SHORT).show();
+        }
 
 
     }
 
 
+}
 
-        //        buttonID = (ImageButton) findViewById(R.id.button00);
-//        buttonID.setOnClickListener(imgA);
-//
-//        buttonIDtest = (ImageButton) findViewById(R.id.button01);
-//        buttonIDtest.setOnClickListener(imgB);
-    }
-
-//    View.OnClickListener imgA = new View.OnClickListener() {
-//        @Override
-//        public void onClick(View view) {
-//            buttonID.setImageResource(R.drawable.x);
-//        }
-//    }
-//
-//    View.OnClickListener imgB = new View.OnClickListener() {
-//        @Override
-//        public void onClick(View view) {
-//            buttonIDtest.setImageResource(R.drawable.check);
-//        }
-//    }
